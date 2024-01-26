fdf1c423b9bab52871c5e5a20c91c95aa4539bea
Diego <you@example.com>
2024-01-26 14:06:32 +0100

    Namen von der API eingespielt

--- React_4bwi/Aufgabe_Card_Website/aufgabe_card_website/src/components/PeopleContainer.js
+++ React_4bwi/Aufgabe_Card_Website/aufgabe_card_website/src/components/PeopleContainer.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./card";
 
 export default function PeopleContainer() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch("https://5e391b0baad2220014962337.mockapi.io/swp/person").then(
+      (res) =>
+        res.json().then((data) => {
+          setPeople(data);
+        })
+    );
+  }, []);
+
   return (
-    <div className="">
+    <div>
       <h1>People</h1>
       <div className="w-40">
-        <Card
-          name="hans"
-          title="CEO"
-          imageUri="https://picsum.photos/200/300"
-        />
+        {people.map((element) => {
+          return <div>{element.name}</div>;
+        })}
       </div>
     </div>
   );
--- React_4bwi/Aufgabe_Card_Website/aufgabe_card_website/src/components/card.js
+++ React_4bwi/Aufgabe_Card_Website/aufgabe_card_website/src/components/card.js
@@ -2,13 +2,17 @@ import React from "react";
 
 export default function Card({ name, title, imageUri }) {
   return (
-    <div className="border grid grid-cols2 h-40">
+    <div className="border grid grid-cols2 h-40 ">
       <div className="p-4">
         <h2 className="font-bold text-xl">{name}</h2>
         <h4>{title}</h4>
       </div>
-      <div>
-        <img src={imageUri} alt="" className="object-cover w-full h-full" />
+      <div className="bg-green-400 w-full">
+        <img
+          src={imageUri}
+          alt="example"
+          className="object-cover w-full h-full"
+        />
       </div>
     </div>
   );
