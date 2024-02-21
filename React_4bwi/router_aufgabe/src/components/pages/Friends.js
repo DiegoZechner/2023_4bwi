9c62bb936398b5ab2caa4dd536933f05ece91689
Diego <you@example.com>
2024-02-21 11:06:49 +0100

    Router aufgesetzt

---
+++ React_4bwi/router_aufgabe/src/components/pages/Friends.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Friends() {
+  return (
+    <div>
+      <h1>friends</h1>
+      <Link to="/">People</Link>
+    </div>
+  );
+}
